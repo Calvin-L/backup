a446bb9deaa6535d06b29c16df5d13fb5f6353ff
Calvin Loncaric <loncaric@cs.washington.edu>
2019-03-23 16:29:48 -0700

    Revert "Better compression"
    This reverts commit 5c976447a516701dbf496c9e8c4ae567980c450f.
    
    XZ uses a huge amount of memory in maximum-compression mode, so to
    keep resource usage moderate I am going to revert this.
    
    In the future, it might be worth reinstating this commit along with
    higher maximum heap size for both the application and the tests.

--- src/main/java/cal/prim/transforms/XZCompression.java
+++ src/main/java/cal/prim/transforms/XZCompression.java
@@ -2,7 +2,6 @@ package cal.prim.transforms;
 
 import cal.bkup.Util;
 import org.tukaani.xz.LZMA2Options;
-import org.tukaani.xz.UnsupportedOptionsException;
 import org.tukaani.xz.XZInputStream;
 import org.tukaani.xz.XZOutputStream;
 
@@ -12,19 +11,7 @@ import java.io.OutputStream;
 
 public class XZCompression implements BlobTransformer {
 
-  private final LZMA2Options options;
-
-  public XZCompression() {
-    LZMA2Options options;
-    try {
-      options = new LZMA2Options(LZMA2Options.PRESET_MAX);
-    } catch (UnsupportedOptionsException e) {
-      System.err.println("XZ library does not support PRESET_MAX compression level!");
-      System.err.println("Using the default compression level instead.");
-      options = new LZMA2Options();
-    }
-    this.options = options;
-  }
+  private final LZMA2Options options = new LZMA2Options();
 
   @Override
   public InputStream apply(InputStream data) {
