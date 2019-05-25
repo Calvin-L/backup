7bc8dd4d62fa44707238acc1a8294914c5862a6e
Calvin Loncaric <marvinx03@gmail.com>
2019-05-25 13:25:53 -0700

    Added another docstring

--- src/main/java/cal/prim/IOConsumer.java
+++ src/main/java/cal/prim/IOConsumer.java
@@ -2,6 +2,11 @@ package cal.prim;
 
 import java.io.IOException;
 
+/**
+ * A {@link java.util.function.Consumer Consumer} whose {@link #accept(Object)} method
+ * may throw {@link IOException}.
+ * @param <T>
+ */
 @FunctionalInterface
 public interface IOConsumer<T> {
   void accept(T x) throws IOException;
